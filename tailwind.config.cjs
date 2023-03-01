ffd425f6b48ea26cc63713b2146c27ece315c6f3
DanielDartDev <olamideonabiyi@student.oauife.edu.ng>
2023-03-01 15:19:48 +0100

    Hero section

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,21 @@
 module.exports = {
   content: ['./index.html'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        canela_trial: ['canela_trial', 'sans-serif'],
+        inter: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        primary: '#D3F85A',
+        secondary: '#131E3A',
+        tertiary: '#FFFFFD',
+        accent: '#090F1D',
+      },
+      screens: {
+        xxl: '1400px',
+      },
+    },
   },
   plugins: [],
 };
